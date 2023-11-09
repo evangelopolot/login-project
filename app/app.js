e222376d479456c00e00b20b654b0acb37eca7f6
Evangel Opolot <59126083+evangelopolot@users.noreply.github.com>
2023-11-09 16:24:33 +0000

    Re-designed the signIn page

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const loginRoute = require('./routes/loginRoute')
+const path = require("path");
 const morgan = require('morgan')
 
 
@@ -10,7 +11,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(bodyParser.urlencoded());
-app.use(express.static('public'));
+
+// Gives access to all the files in the publlic folder and not just the htmml being send.
+// Does the file matching.
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.get('/', loginRoute);
