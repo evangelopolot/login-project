4cf752148ed382b598d0abfaf249cd0f8ad0ed05
Evangel <evangelopolot@gmail.com>
2023-11-22 13:42:30 +0000

    Working login

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,58 +1,88 @@
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
-const jsonData = require('../data/data.json');
-const users = require("../data/users.json");
-const { log } = require('console');
+const users = require("../data/data.json");
+const { log } = require("console");
 
 exports.homepage = (req, res) => {
-    res.render('index', {title: 'Home'})
-}
+  res.render("index", { title: "Home" });
+};
 
 exports.signIn = (req, res) => {
-    res.render('signIn', {title: 'Sign In'})
-}
+  res.render("signIn", { title: "Sign In" });
+};
 
 exports.signUp = (req, res) => {
-    res.render('signUp', {title: 'Sign Up'})
-}
+  res.render("signUp", { title: "Sign Up" });
+};
 
 exports.login = (req, res) => {
-    console.log('Post has been made.')
-    console.log(req.body);
+  const filePath = path.join(__dirname, "../", "data", "data.json");
 
-    let email = JSON.stringify(req.body.email);
-    const low = check(req.body.email);
-    console.log(low);
-    let userPassword = JSON.stringify(req.body.password)
-    const newData = {
-        email: email,
-        password: req.body.password,
-    };
-    console.log(email,userPassword);
-    users.data.forEach((user) => {
-        if (user.body.email == newData.email & user.body.password === newData.password){
-            console.log("We have a match");
-        }
-    })
-    res.status(201).json(
-        { 'message': 'Data recieved successfully' });
-}
- 
+  let email = JSON.stringify(req.body.email);
+  let userPassword = JSON.stringify(req.body.password);
+  const loginData = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+  console.log(email, userPassword);
+  console.log("The data", users);
+  users.data.forEach((user) => {
+    if (
+      (user.email === loginData.email) &
+      (user.password === loginData.password)
+    ) {
+      console.log("We have a match");
+    } else {
+      console.log("No match found!");
+    }
+  });
+  res.render("signIn", { title: "Sign In" });
+};
+
 exports.createUser = (req, res) => {
+  // Path to the JSON file
+  const filePath = path.join(__dirname, "../", "data", "data.json");
+
+  // Read existing JSON data
+  fs.readFile(filePath, "utf-8", (readErr, data) => {
+    if (readErr) {
+      console.error("Error reading JSON file:", readErr);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+
+    // Parse the existing JSON data
+    let jsonData;
+    try {
+      jsonData = JSON.parse(data);
+    } catch (parseErr) {
+      console.error("Error parsing JSON:", parseErr);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+
+    // Append new user data
     const newUserData = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
     };
-    console.log(newUserData);
+
     jsonData.data.push(newUserData);
-    
-    fs.writeFile(path.join(__dirname, "../", "data", "data.json"), JSON.stringify(newUserData), (err) => {
-        if (err) {
-            return console.log(err);
+
+    // Write the updated JSON data back to the file
+    fs.writeFile(
+      filePath,
+      JSON.stringify(jsonData, null, 2),
+      "utf8",
+      (writeErr) => {
+        if (writeErr) {
+          console.error("Error writing to JSON file:", writeErr);
+          return res.status(500).json({ message: "Internal Server Error" });
         }
-    });
-    res.status(201).json(
-        { 'message': 'Data recieved successfully' });
-}
+
+        console.log("Data appended to JSON file successfully.");
+        res.render("signIn", { title: "Sign In" });
+      }
+    );
+  });
+};
