59ac01d56e4d77305849f91a032ca97fcfc4d3e4
Evangel Opolot <evangel.opolot@engineering.digital.dwp.gov.uk>
2024-01-04 15:55:18 +0000

    feat: improve application structure

--- app/public/scripts/css.js
+++ app/public/scripts/css.js
@@ -5,3 +5,17 @@ accordionItemHeaders.forEach(accordionItemHeader => {
         accordionItemHeader.classList.toggle("active")
     });
 });
+
+const title = document.querySelector(".card__title"); 
+const description = document.querySelector(".card__description"); 
+const image = document.querySelector(".card__image"); 
+      
+// Remove the 'loading' class 
+title.classList.remove("loading"); 
+description.classList.remove("loading"); 
+image.classList.remove("loading"); 
+      
+// Add the content 
+title.textContent = 'Title'; 
+description.textContent = 'This is the description.'; 
+image.innerHTML = `<img src="${image-source}`; 
